d51516b7bb87f127033dde61bdbf39b30bd457e9
elias <defreitase@bzz.ch>
2023-12-11 11:57:04 +0100

    French

---
+++ French/scripts_fr.js
@@ -0,0 +1,22 @@
+    document.addEventListener("DOMContentLoaded", () => {
+    let moreContentButton = document.getElementById("moreContentButton")
+
+    moreContentButton.addEventListener("click", () => {
+        let additionalYears = document.querySelectorAll(".additional-year");
+
+        additionalYears.forEach(function (year) {
+            year.style.display = year.style.display === "none" || year.style.display === "" ? "block" : "none";
+        });
+        moreContentButton.innerText = moreContentButton.innerText === "Voir plus" ? "Voir moins" : "Voir plus";
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+    let moreCorpsButton = document.getElementById("additional-corp-button")
+    moreCorpsButton.addEventListener("mousedown", () => {
+        let moreCorps = document.querySelectorAll(".additional-corp");
+        moreCorps.forEach(function(corp) {
+            corp.style.display = corp.style.display === "none" || corp.style.display === "" ? "table-row" : "none";
+        });
+        moreCorpsButton.innerText = moreCorpsButton.innerText === "Voir plus" ? "Voir moins" : "Voir plus";
+    })
+});
